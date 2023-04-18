a1a14e4c0a354c17347fdcea9f702805747b3a42
joaquinbian <joacobian21@gmail.com>
2023-04-18 01:14:35 -0300

    stretch goal, restart game

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Banner({ gameStatus, answer, guesses }) {
+function Banner({ gameStatus, answer, guesses, onRestart }) {
   if (gameStatus === "lost") {
     return (
       <div className="sad banner">
         <p>
           Sorry, the correct answer is <strong>{answer}</strong>.
         </p>
+        <button onClick={onRestart}>restart game</button>
       </div>
     );
   }
@@ -15,6 +16,7 @@ function Banner({ gameStatus, answer, guesses }) {
       <p>
         <strong>Congratulations!</strong> Got it in
         <strong>{guesses.length} guesses</strong>.
+        <button onClick={onRestart}>restart game</button>
       </p>
     </div>
   );
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,14 +9,16 @@ import Banner from "../Banner/Banner";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
-export const answer = sample(WORDS);
+
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
   const [results, setResults] = React.useState([]);
   const [gameStatus, setGameStatus] = React.useState("running");
+  const [answer, setAnswer] = React.useState(sample(WORDS));
+
+  console.info({ answer });
 
   const addGuess = (guess) => {
     const nextGuesses = [...guesses, guess];
@@ -30,10 +32,22 @@ function Game() {
     }
   };
 
+  const restartGame = () => {
+    setAnswer(sample(WORDS));
+    setGuesses([]);
+    setResults([]);
+    setGameStatus("running");
+  };
+
   return (
     <>
       {gameStatus !== "running" && (
-        <Banner gameStatus={gameStatus} answer={answer} guesses={guesses} />
+        <Banner
+          gameStatus={gameStatus}
+          answer={answer}
+          guesses={guesses}
+          onRestart={restartGame}
+        />
       )}
       <Guesses guesses={guesses} results={results} />
       <GuessForm disabled={gameStatus !== "running"} addGuess={addGuess} />
